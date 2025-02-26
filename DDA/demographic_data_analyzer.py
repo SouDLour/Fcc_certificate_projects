7dc21b6c3ba04606d9c010094bfd736beb5df341
SouDLour <132702113+SouDLour@users.noreply.github.com>
2025-02-26 14:03:24 -0800

    updated needed files

--- DDA/demographic_data_analyzer.py
+++ DDA/demographic_data_analyzer.py
@@ -4,6 +4,7 @@ import pandas as pd
 def calculate_demographic_data(print_data=True):
     # Read data from file
     df = pd.read_csv('adult_data.csv')
+    ddf = pd.read_csv('adult_data.csv')
 
     # How many of each race are represented in this dataset? This should be a Pandas 
     # series with race names as the index labels.
@@ -24,49 +25,96 @@ def calculate_demographic_data(print_data=True):
     total_bach_count = total_bach.count()
     percent_of_bach = (total_bach_count/ total_education) * 100
     
-    percentage_bachelors = percent_of_bach 
+    percentage_bachelors = percent_of_bach.round()
 
     # What percentage of people with advanced education (`Bachelors`, `Masters`, or `Doctorate`) make more than 50K?
-    # What percentage of people without advanced education make more than 50K?
-
-    ###RICH ADVANCED EDUCATION HIGHER EDUCATION
-    higher_education_rich = pd.DataFrame(df)
-    higher_education_rich.loc[higher_education_rich['education'].isin(['Bachelors','Masters','Doctorate']),['salary','education']]
-    total_higher_edu = higher_education_rich.education.value_counts().sum()
-
-    smart_rich = higher_education_rich.loc[(higher_education_rich['education'].isin(['Bachelors','Masters','Doctorate'])) & (higher_education_rich['salary'] == '<=50K'),['salary','education']]
-    total_high_50 = smart_rich.value_counts().sum()
+    main_edu = df['education']
+    main_tot = main_edu.value_counts().sum()
+    higher_education_rich_m = pd.DataFrame(df)
+    filt_high_edu = higher_education_rich_m.loc[(higher_education_rich_m.education.isin(['Bachelors','Masters','Doctorate']) & (higher_education_rich_m['salary']=='<=50K')),['salary','education']]
+    high_total = filt_high_edu.value_counts().sum()
+    percent_back = (high_total / main_tot) * 100
     
-    percent_rich = total_high_50 / total_higher_edu * 100
-    high_edu_rich_perc = percent_rich.round() 
-
-    ###POOR PERCENTAGE PEOPLE ADVANCE EDUCATION LESS THAN 50K
+    # [REDO THIS ONE ]What percentage of people without advanced education make more than 50K? NO NEED
+    lower_education_rich_m = pd.DataFrame(df)
+    filt_lower_edu = lower_education_rich_m.loc[(lower_education_rich_m.education.isin(['Bachelors','Masters','Doctorate']) & (lower_education_rich_m['salary']=='>50K')),['salary','education']]
+    lower_total = filt_lower_edu.value_counts().sum()
+    percent_back_low = (lower_total / main_tot) * 100
 
     # with and without `Bachelors`, `Masters`, or `Doctorate`
-    higher_education = high_edu_rich_perc
-    lower_education = None
+    higher_education = percent_back
+    lower_education = percent_back_low
+    
+    # Highed Education but less > 50K
+    high_less = pd.DataFrame(df)
+    filt_high_less = high_less.loc[(high_less.education.isin(['Bachelors','Masters','Doctoate']) & (high_less['salary'] == '>50K')),['salary','education']]
+    fhl = filt_high_less.value_counts().sum()
+    high_less_percent = (fhl/main_tot)*100
+    high_less_percent
+    
+    #Lower Education Rich Code:
+    original_amount = pd.DataFrame(df)
+    dividen_amount = original_amount['education'].value_counts().sum()
+    dividen_amount
+    
+    # Turning Any cell that has the string_data into '' <= making it a new value for the column
+    without_edu_low = pd.DataFrame(df)
+    string_data = ['Bachelors','Doctorate','Masters']
+    without_edu_low['education'] = without_edu_low['education'].apply(lambda x: ''.join([word for word in x.split() if word not in string_data]))
+    without_edu_low
+    
+    # Dropping the '' Data from the column thus removing every row
+    for x in without_edu_low.index:
+        if without_edu_low.loc[x,'education'] == '':
+            without_edu_low.drop(x, inplace=True)
+
+    # Getting Percent of people with less education less than 50K
+    num_no_high_less = without_edu_low['education'].value_counts().sum()
+    calc_non_less = (num_no_high_less/dividen_amount) * 100
+    
+
 
     # percentage with salary >50K
-    higher_education_rich = None
-    lower_education_rich = None
+    higher_education_rich = high_less_percent.round()
+    lower_education_rich = calc_non_less.round()
 
     # What is the minimum number of hours a person works per week (hours-per-week feature)?
-    min_work_hours = None
+    df['hours-per-week'].info()
+    df['hours-per-week'].describe()
+    min_hr = df['hours-per-week'].min()
+    min_work_hours = min_hr
 
     # What percentage of the people who work the minimum number of hours per week have a salary of >50K?
-    num_min_workers = None
+    ppl_work_1hr =  df.loc[(df['hours-per-week'] == 1) & (df['salary'] == '>50K'),['hours-per-week','salary']]
+    total_ppl = df['hours-per-week'].value_counts().sum()
+    
+    percentag_sum = (ppl_work_1hr.value_counts().sum()/total_ppl) * 100
+    
+    num_min_workers = ppl_work_1hr.value_counts()
 
-    rich_percentage = None
+    rich_percentage = percentag_sum
 
-    # What country has the highest percentage of people that earn >50K?
-    highest_earning_country = None
-    highest_earning_country_percentage = None
+    # What Country with highest percentage of rich & Highest percentage of rich people in country?
+    richest_country = df[['salary','native-country']].value_counts()
+    highest_earning_country = richest_country
+    
+    ppl_united_plus50 = df.loc[(df['native-country']=='United-States') & (df['salary']=='<=50K'),['salary','native-country']]
+    ppl_united_plus50.isna().any()
+    a_ppl = ppl_united_plus50.value_counts().sum()
+    percentageRichPpl = (a_ppl / 32561) * 100
+    percentageRichPpl
+    highest_earning_country_percentage = percentageRichPpl
 
     # Identify the most popular occupation for those who earn >50K in India.
-    top_IN_occupation = None
+    ddf[['occupation','native-country','salary']].value_counts()
+    ddf['native-country'].value_counts()
+    top_IN = ddf.loc[(ddf['native-country']=='India') & (ddf['education']==ddf['education']),['salary','occupation']].value_counts().idxmax()
+    
+    top_IN_occupation = top_IN
+
+
 
     # DO NOT MODIFY BELOW THIS LINE
-
     if print_data:
         print("Number of each race:\n", race_count) 
         print("Average age of men:", average_age_men)
